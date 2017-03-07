815a970ba539469bd3d508229c4a977d7882fc55
Robert May <robertmay1000@gmail.com>
2017-03-07 20:48:49 +0000

    Use react-bootstrap for message list

--- src/views/MessageList.js
+++ src/views/MessageList.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
 import _ from 'lodash';
 
 export class MessageList extends Component {
   render() {
-    return React.createElement('ul', {}, 
+    return React.createElement(ListGroup, {}, 
       _.map(this.props.messages, (message => {
-          return React.createElement('li', {}, message);
+          return React.createElement(ListGroupItem, {}, message);
       }))
     )
   }
